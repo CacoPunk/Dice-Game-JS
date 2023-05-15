771f7d043adb61adff7a504155c902e952a8f9ea
Paulo Ricardo Silva dos Santos Filho <desenvolvimento4@codi.com.br>
2023-05-15 16:51:32 -0300

    Adicionado mensagem do vencedor.

--- jogoDados/app.js
+++ jogoDados/app.js
@@ -11,8 +11,17 @@ dados.forEach(function(img, i) {
 
 rolar.addEventListener("click", function(){
     let dados = document.querySelectorAll("img");
+    let score = [0, 0];
      dados.forEach(function(img, i) {
-        img.setAttribute("src", getDiceNumber())
+        let temp = getDiceNumber();
+        score[i] = temp.slice(11, 12);
+        img.setAttribute("src", temp);
     })
-
+    if (score[0] > score[1]) {
+        document.querySelector(".winner").innerHTML = "Jogador 1 VENCEU!"
+    } else if(score[0] < score[1] ) {
+        document.querySelector(".winner").innerHTML = "Jogador 2 VENCEU!"
+    } else {
+        document.querySelector(".winner").innerHTML = "EMPATE"
+    }
 }  )
